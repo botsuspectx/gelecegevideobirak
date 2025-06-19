110c968e0e0d81bfce6de732d1863644cb8bdf82
botsuspectx <93864487+botsuspectx@users.noreply.github.com>
2025-06-20 00:55:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -121,7 +121,8 @@ app.post("/shopier-odeme", (req, res) => {
     };
 
     const ordered = Object.entries(data).sort();
-    const signatureStr = ordered.map(([key, val]) => `${key}=${val}`).join("&") + "&" + secretKey;
+    // 🔄 Doğru hali
+    const signatureStr = ordered.map(([key, val]) => `${key}=${val}`).join("&") + secretKey;
     const signature = crypto.createHash("sha256").update(signatureStr).digest("hex");
 
     console.log("Shopier data:", data);
